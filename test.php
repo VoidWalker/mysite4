4f44a138a7b39d773c9169d98dcf3b5c600eac5f
VoidWalker <sohanoleksa@gmail.com>
2014-07-22 19:01:45 +0300

    Second commit

---
+++ test.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sasha
+ * Date: 22.07.14
+ * Time: 18:39
+ */
+class Author{
+    private $fname;
+    private $lname;
+
+    public function _construct($fn, $ln){
+        $this->fname = $fn;
+        $this->lname = $ln;
+
+    }
+
+    function getFName(){/**/}
+    function getLName(){/**/}
+
+
+}
+
+class Book{
+    private $_title;
+    private $_author;
+
+    public function _construct($t, Author $a){
+        $this->_title = $t;
+        $this->_author = $a;
+
+    }
+    function getAuthor(){/**/}
+    function getTitle(){/**/}
+}
+?>
